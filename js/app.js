55ba68cf5f971592868231738f6db8e847448e39
Adam Bergen <bergen@gmail.com>
2014-10-20 22:25:41 -0500

    blank page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 _.templateSettings.interpolate = /{([\s\S]+?)}/g;
 
+window.onload = app;
+
+function app() {
+    
+var options = {api_key: "4jls0ietsf4fdx1hkkdghcie"}
+
+var Etsy = new EtsyClient(options);
+
+}
+
+
 
 //setting up the constructor
 function EtsyClient(options) {
@@ -14,11 +25,11 @@ function EtsyClient(options) {
     this.init();
 }
 
-EtsyClient.prototype.PullAllActiveListings = function() {
+EtsyClient.prototype.pullAllActiveListings = function() {
     var model = 'listings/'
     var filter = 'active';
 
-    return $.getJSON(this.complete_api_url + model + filter + ".js?api_key=" + this.api_key + "&callback=?").then(function(data) {
+    return $.getJSON(this.complete_api_url + model + filter + ".js?api_key=" + this.api_key + "&includes=Images&callback=?").then(function(data) {
         //console.log(data);
         return data;
     });
@@ -31,26 +42,57 @@ EtsyClient.prototype.loadTemplateFile = function(templateName) {
 }
 
 EtsyClient.prototype.putListingsOnPage = function(listingsHtml, listings) {
-    document.querySelector('#templateDestination').innerHTML = listings.results.map(function(listing){
+    document.querySelector('#templateDestination').innerHTML = listings.results.map(function(listing){ //what does listings.results.map do?
     	return _.template(listingsHtml, listing);
     }).join('');
 }
 
+EtsyClient.prototype.putOneListingOnPage = function(id){
+	var listing = this.latestData.results.filter(function(listing){ //no clue what this entire thing does really
+        return listing.listing_id === parseInt(id);
+    });
+
+	document.querySelector('#templateDestination').innerHTML = _.template(this.singleListingHtml, listing[0]); //listing at 0??
+}
+
+EtsyClient.prototype.setupRouting = function(){
+	var self = this;
+
+	Path.map("/#").to(function(){
+		self.putListingsOnPage(self.listingHtml, self.latestData)  //where is self.latestData coming from? 
+	});
+
+	Path.map("#/message/:anymessage").to(function(){
+        alert(this.params.anymessage);
+    })
+
+    Path.map("#/listing/:id").to(function() {
+        self.putOneListingOnPage(this.params.id);
+    });
+
+    Path.root("#/");
+
+}
+
+
+
 EtsyClient.prototype.init = function() {
     var self = this;
+    this.setupRouting();
 
     $.when(
-        this.PullAllActiveListings(),
-        this.loadTemplateFile('listings')
-    ).then(function(listings, listingsHtml) {
-        self.putListingsOnPage(listingsHtml, listings)
+        this.pullAllActiveListings(),
+        this.loadTemplateFile('listings'),
+        this.loadTemplateFile('onelisting')
+    ).then(function(listings, listingsHtml, onelistingHtml) {
+    //    self.putListingsOnPage(listingsHtml, listings)
+     	self.latestData = listings;
+        self.listingHtml = listingsHtml;
+        self.singleListingHtml = onelistingHtml;
+
+        Path.listen();
+
     })
 }
 
-window.onload = app;
 
-function app() {
-    var Etsy = new EtsyClient({
-        api_key: "4jls0ietsf4fdx1hkkdghcie"
-    });
-}
